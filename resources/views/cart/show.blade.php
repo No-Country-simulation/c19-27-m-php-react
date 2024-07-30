b8ca4116141271d15f54c4ea80d44a442131fefe
Josue Colindres <113657563+maguilera4323@users.noreply.github.com>
2024-07-30 12:38:42 -0600

    correccion carrito

--- resources/views/cart/show.blade.php
+++ resources/views/cart/show.blade.php
@@ -77,11 +77,11 @@
                 </a>
                 <div class="px-5 pb-5">
                     <a href="#">
-                        <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ $product->name }}</h5>
+                        <h5 class="text-xl font-semibold tracking-tight text-gray-900 ">{{ $product->name }}</h5>
                     </a>
                     <p class="text-gray-700">{{ $product->description }}</p>
                     <div class="flex items-center justify-between mt-2.5 mb-5">
-                        <span class="text-lg font-bold text-gray-900 dark:text-white">${{ $product->price }}</span>
+                        <span class="text-lg font-bold text-gray-900 ">${{ $product->price }}</span>
                         <div class="flex items-center space-x-1 rtl:space-x-reverse">
                             <svg class="w-3 h-3 text-neon-green" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                                 <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
@@ -101,7 +101,7 @@
                         </div>
                     </div>
                     <div class="flex items-center justify-between">
-                        <button class="btn btn-success" onclick="addToCart({{ $product->id }})" style="border-radius: 6px; background: #007BFF; color:white; width: 157.355px; height: 31.857px;">Agregar al carrito</button>
+                        <button class="btn btn-success" onclick="addToCart('{{ $product->id }}')" style="border-radius: 6px; background: #007BFF; color:white; width: 157.355px; height: 31.857px;">Agregar al carrito</button>
                     </div>
                 </div>
             </div>
